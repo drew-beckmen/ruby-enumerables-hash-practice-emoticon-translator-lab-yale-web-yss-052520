4182e147e03353495825328086a3a456860e881d
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-09 22:29:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emojis = YAML.load_file(path)
+  restructured_hash = {}
+  emojis.each do |key, array_emojis|
+    emojis[key] = { english: array_emojis[0], japanese: array_emojis[1] }
+  end 
+  emojis
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(emoji)
+  dictionary = load_library
+  dictionary.each do |english_meaning, translations|
+    if translation[:japanese] == emoji 
+      return english_meaning
+    end 
+  end 
 end
+
+puts load_library("./lib/emoticons.yml")
